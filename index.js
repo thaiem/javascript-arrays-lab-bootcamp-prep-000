1bda0b147a2e70fdbb5f08a2ba105297d77dbd2d
Elaine Thai <ethai1827@gmail.com>
2019-05-14 21:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name) {
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  
+}
